d210b333f3edf8454dbc7211c12c8dc4d07e04f8
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-03-27 17:50:24 +1100

    - Added the update method to the model class. This will accept a hashmap of values and update the ones that valid values for this model.

--- src/main/java/com/plantiq/plantiqserver/core/Model.java
+++ src/main/java/com/plantiq/plantiqserver/core/Model.java
@@ -11,4 +11,48 @@ public class Model {
         this.data = data;
     }
 
+
+    //|================================================|
+    //|              STATIC CRUD METHODS               |
+    //|================================================|
+
+    //Our static CRUD methods map to the crud actions of
+    //create, read, update and delete, these are all assigned
+    //to a static that and will wrap around any other classes
+
+    //------------------ UPDATE METHOD -----------------
+
+    //This method accepts a hash map of data and will validate
+    //that the keys are present in the model object, if so
+    //then it will build a query to update then
+    public boolean update(HashMap<String,String> data){
+
+        //Create our query string and initialize it to the starting value
+        StringBuilder query = new StringBuilder("UPDATE "+data.get("_table")+" SET");
+
+        data.forEach((key,value)->{
+
+            //Check if the data in our data hashmap matches a key present
+            //in this object, if so then append the key value to the query.
+            if(this.data.containsKey(key)){
+                query.append(" "+key+"='"+value+"'");
+            }
+
+        });
+
+        //Next append our where query to update this record only
+        query.append(" WHERE id="+this.data.get("id"));
+
+        //Finally return our result as a boolean
+        return Database.getAndResetRowsAffected() != 0;
+    }
+
+    public void patch(HashMap<String,String> date){
+
+    }
+
+    public void delete(){
+
+    }
+
 }
