fc6c90889a555f46cabc02cb9e4852eda7b3411c
s3907260@student@rmit.edu.au <s3907260@student@rmit.edu.au>
2023-03-27 15:31:16 +1100

    Parent model class for sensors and SmartHomeHub

---
+++ src/main/java/com/plantiq/plantiqserver/core/Model.java
@@ -0,0 +1,14 @@
+package com.plantiq.plantiqserver.core;
+
+import java.util.HashMap;
+
+
+public class Model {
+
+    protected HashMap<String,String> data;
+
+    public Model(HashMap<String, String> data){
+        this.data = data;
+    }
+
+}
