eca5fa9159cd169e13672e766cc87847f901dc29
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-03-27 17:56:13 +1100

    - Removed database test code.

--- src/main/java/com/plantiq/plantiqserver/PlantIqServerApplication.java
+++ src/main/java/com/plantiq/plantiqserver/PlantIqServerApplication.java
@@ -1,33 +1,14 @@
 package com.plantiq.plantiqserver;
 
 
-import com.plantiq.plantiqserver.model.Sensor;
-import com.plantiq.plantiqserver.model.SmartHomeHub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
 
 
 @SpringBootApplication
 public class PlantIqServerApplication {
 
-
 	public static void main(String[] args) {
-
-
-		ArrayList<SmartHomeHub> results = SmartHomeHub.collection().get();
-
-		results.forEach((n)->{
-			System.out.println(n.getName());
-		});
-
-		ArrayList<Sensor> results2 = Sensor.collection().get();
-		results2.forEach((n)->{
-			System.out.println(n.getSensorType());
-		});
-
-		//System.exit(0);
 
 		SpringApplication.run(PlantIqServerApplication.class, args);
 	}
