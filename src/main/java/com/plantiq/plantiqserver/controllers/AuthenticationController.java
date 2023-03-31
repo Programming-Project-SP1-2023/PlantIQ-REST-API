7a8796201f3848532f6d8ad8bab5ef0d8093883c
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-03-31 18:17:52 +1100

    - Created AuthenticationController.java and fleshed out starter login method.

---
+++ src/main/java/com/plantiq/plantiqserver/controllers/AuthenticationController.java
@@ -0,0 +1,54 @@
+package com.plantiq.plantiqserver.controllers;
+
+import com.plantiq.plantiqserver.model.User;
+import com.plantiq.plantiqserver.rules.LoginRequestRule;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @PostMapping("/login")
+    public HashMap<String,Object> login(HttpServletRequest request) {
+
+        //Create our response object, this is returned as JSON.
+        HashMap<String, Object> response = new HashMap<>();
+
+        //Create our login request rule, we will validate our request
+        //using this rule.
+        LoginRequestRule rule = new LoginRequestRule();
+
+        //Validate our request and return the errors if present.
+        if(!rule.validate(request)){
+            response.put("outcome",false);
+            response.put("error",rule.getErrors());
+            return response;
+        }
+
+        //Else if we reach this stage of the code then proceed to
+        //lookup the user from the database.
+        User user = User.collection().where("email",request.getParameter("email")).where("password",request.getParameter("password")).getFirst();
+
+        if(user != null){
+            response.put("outcome","true");
+        }else{
+            response.put("outcome","false");
+            response.put("error","Login failed, please check your email and password");
+        }
+
+        return response;
+    }
+
+    @PostMapping("/logout")
+    public HashMap<String, String> logout(){
+
+
+
+        return null;
+    }
+}
