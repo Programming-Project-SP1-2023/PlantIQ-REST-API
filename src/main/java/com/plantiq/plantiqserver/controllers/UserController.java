7a9f869ee57edae72661166bbae33c82d40f7490
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-03-31 18:15:29 +1100

    - Removed login method from UserController.java, this now sits in the AuthenticationController.java

--- src/main/java/com/plantiq/plantiqserver/controllers/UserController.java
+++ src/main/java/com/plantiq/plantiqserver/controllers/UserController.java
@@ -2,10 +2,12 @@ package com.plantiq.plantiqserver.controllers;
 
 import com.plantiq.plantiqserver.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -18,18 +20,5 @@ public class UserController {
             return User.collection().get();
         }
 
-        public void login(String email ,String password) {
-
-            ArrayList<User> output = User.collection().where("email","Sil").where("password","123").get();
-
-            if (output.size()==0){
-                //No Match, Stay On the login Page
-            }
-
-            else{
-                // Account exists, Move to HomePage/DashBoard
-            }
-
-        }
 
 }
