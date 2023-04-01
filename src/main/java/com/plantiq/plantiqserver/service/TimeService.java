5054cc2b48aec00177103b132c0939af1f84278b
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-01 15:08:15 +1100

    Commit content created by Alex Vorraro. - Created nowPlusDays(int days) method, this accepts a day count and will return the unix timestamp for then. - Create now() method, returns the current unix time stamp.

---
+++ src/main/java/com/plantiq/plantiqserver/service/TimeService.java
@@ -0,0 +1,18 @@
+package com.plantiq.plantiqserver.service;
+
+//Class created to obtain current timestamps and future timestamps in UNIX format
+public class TimeService {
+
+    //This method is used to gain the timestamp of a future day.
+    //It will get the current timestamp and sum an offset of days received as the parameter.
+    public static Long nowPlusDays(int offset){
+
+        return TimeService.now() + offset*(24*3600);
+    }
+
+    //This method returns the current timestamp.
+    public static Long now(){
+        return System.currentTimeMillis() / 1000L;
+    }
+
+}
