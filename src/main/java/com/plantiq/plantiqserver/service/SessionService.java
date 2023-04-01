63e21d90c53bd61f0ef4990bffe28a567ea936a2
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-01 15:47:07 +1100

    - Updated the SessionService::create static method to now delete any lingering sessions from the table before inserting the new one. - Improved overall commenting.

--- src/main/java/com/plantiq/plantiqserver/service/SessionService.java
+++ src/main/java/com/plantiq/plantiqserver/service/SessionService.java
@@ -4,6 +4,7 @@ import com.plantiq.plantiqserver.model.Session;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SessionService {
@@ -12,22 +13,37 @@ public class SessionService {
     //SHA256 hash of the user_id and the current time.
     public static String create(String user_id){
 
+        //Delete any previous sessions the user may have already created, to
+        //ensure that we do not end up with any duplicated sessions we will
+        //get an arraylist of them and delete all entries, this should be 1
+        //only.
+        ArrayList<Session> sessions = Session.collection().where("user_id",user_id).get();
+
+        //Delete all sessions for this user.
+        sessions.forEach((n)-> n.delete("token"));
+
+        //Use the user_id and time to create the hash value.
         String data = user_id+TimeService.now();
         String hash = hashUsingSHA1(data);
 
+        //Create the new session object data map.
         HashMap<String,Object> sessionData = new HashMap<>();
 
         sessionData.put("token",hash);
         sessionData.put("user_id",user_id);
         sessionData.put("expiry",TimeService.nowPlusDays(14));
+
+        //Insert the new session into the database
         boolean outcome = Session.insert("Session",sessionData);
 
+        //If this fails then print to console.
         if(!outcome){
             System.out.println("[SessionService] failed to insert session key into database!");
         }
 
+        //Lastly we return the hash value back to the caller, this allows
+        //for the front end to get it!
         return hash;
-
     }
 
 
