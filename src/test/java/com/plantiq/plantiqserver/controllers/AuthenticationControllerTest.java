5a44d8b5974053f5958be5ef6afee5d831a6a2b6
Falcons34 <89521528+Falcons34@users.noreply.github.com>
2023-04-03 16:01:08 +1000

    Test file added for rules and started controller endpoints

---
+++ src/test/java/com/plantiq/plantiqserver/controllers/AuthenticationControllerTest.java
@@ -0,0 +1,45 @@
+package com.plantiq.plantiqserver.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.util.HashMap;
+
+class AuthenticationControllerTest {
+	@BeforeEach
+	void setUp() {
+
+	}
+
+	@Test
+	void login() {
+	}
+
+	@Test
+	void logout() {
+	}
+
+	@Test
+	void validate() {
+	}
+
+	/**
+	 * Test the registration endpoint.
+	 * User should be able to register with a valid email and password.
+	 */
+	@Test
+	void register() {
+		HttpServletRequest data = new MockHttpServletRequest();
+		data.setAttribute("firstname", "Bill123");
+		data.setAttribute("surname", "Smith");
+		data.setAttribute("email", "bill@gmail.com");
+		data.setAttribute("password", "password1234");
+		// post request to /register
+		AuthenticationController controller = new AuthenticationController();
+		HashMap<String, Object> res = controller.register(data);
+		// assert that the response is successful
+		assert res.get("success").equals(true);
+	}
+}
