f223fb506be1f7e1fbe74f4dac242cf4aee38755
Falcons34 <89521528+Falcons34@users.noreply.github.com>
2023-04-07 15:41:06 +1000

    cross

--- src/main/java/com/plantiq/plantiqserver/controllers/AuthenticationController.java
+++ src/main/java/com/plantiq/plantiqserver/controllers/AuthenticationController.java
@@ -14,114 +14,113 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/auth")
 public class AuthenticationController {
+	@PostMapping("/login")
+	public HashMap<String, Object> login(HttpServletRequest request) {
 
-    @PostMapping("/login")
-    public HashMap<String,Object> login(HttpServletRequest request) {
+		//Create our response object, this is returned as JSON.
+		HashMap<String, Object> response = new HashMap<>();
 
-        //Create our response object, this is returned as JSON.
-        HashMap<String, Object> response = new HashMap<>();
+		//Create our login request rule, we will validate our request
+		//using this rule.
+		LoginRequestRule rule = new LoginRequestRule();
 
-        //Create our login request rule, we will validate our request
-        //using this rule.
-        LoginRequestRule rule = new LoginRequestRule();
+		//Validate our request and return the errors if present.
+		if (!rule.validate(request)) {
+			response.put("outcome", false);
+			response.put("errors", rule.getErrors());
+			return response;
+		}
 
-        //Validate our request and return the errors if present.
-        if(!rule.validate(request)){
-            response.put("outcome",false);
-            response.put("errors",rule.getErrors());
-            return response;
-        }
+		//Else if we reach this stage of the code then proceed to
+		//lookup the user from the database.
+		User user = User.collection().where("email", request.getParameter("email")).where("password", request.getParameter("password")).getFirst();
 
-        //Else if we reach this stage of the code then proceed to
-        //lookup the user from the database.
-        User user = User.collection().where("email",request.getParameter("email")).where("password",request.getParameter("password")).getFirst();
+		if (user != null) {
+			response.put("outcome", "true");
+			response.put("session", SessionService.create(user.getId()));
+		} else {
+			response.put("outcome", "false");
+			response.put("errors", "Login failed, please check your email and password");
+		}
 
-        if(user != null){
-            response.put("outcome","true");
-            response.put("session", SessionService.create(user.getId()));
-        }else{
-            response.put("outcome","false");
-            response.put("errors","Login failed, please check your email and password");
-        }
+		return response;
+	}
 
-        return response;
-    }
+	@DeleteMapping("/logout")
+	public HashMap<String, Object> logout(HttpServletRequest request) {
 
-    @DeleteMapping("/logout")
-    public HashMap<String, Object> logout(HttpServletRequest request){
+		HashMap<String, Object> response = new HashMap<>();
+		SessionValidateRequestRule rule = new SessionValidateRequestRule();
 
-        HashMap<String,Object> response = new HashMap<>();
-        SessionValidateRequestRule rule = new SessionValidateRequestRule();
+		if (!rule.validate(request)) {
+			response.put("outcome", false);
+			response.put("errors", rule.getErrors());
+			return response;
+		}
 
-        if(!rule.validate(request)){
-            response.put("outcome",false);
-            response.put("errors",rule.getErrors());
-            return response;
-        }
+		Session session = Session.collection().where("token", request.getParameter("token")).getFirst();
+		session.delete("token");
 
-        Session session = Session.collection().where("token",request.getParameter("token")).getFirst();
-        session.delete("token");
+		response.put("outcome", true);
 
-        response.put("outcome",true);
+		return response;
+	}
 
-        return response;
-    }
+	@GetMapping("/validate/{token}")
+	public HashMap<String, Object> validate(@PathVariable("token") String token) {
 
-    @GetMapping("/validate/{token}")
-    public HashMap<String, Object> validate(@PathVariable("token")String token){
+		HashMap<String, Object> response = new HashMap<>();
 
-        HashMap<String,Object> response = new HashMap<>();
+		Session session = Session.collection().where("token", token).getFirst();
 
-        Session session = Session.collection().where("token",token).getFirst();
+		if (session == null) {
+			response.put("outcome", false);
+			response.put("errors", "Invalid token provided!");
+			return response;
+		}
 
-        if(session == null){
-            response.put("outcome",false);
-            response.put("errors","Invalid token provided!");
-            return response;
-        }
+		if (session.getExpiry() < TimeService.now()) {
+			session.delete("token");
+			response.put("outcome", false);
+		} else {
+			response.put("outcome", true);
+		}
 
-        if(session.getExpiry() < TimeService.now()){
-            session.delete("token");
-            response.put("outcome", false);
-        }else{
-            response.put("outcome", true);
-        }
+		return response;
+	}
 
-        return response;
-    }
+	@PostMapping("/register")
+	public HashMap<String, Object> register(HttpServletRequest request) {
+		HashMap<String, Object> response = new HashMap<>();
+		RegistrationRequestRule rule = new RegistrationRequestRule();
 
-    @PostMapping("/register")
-    public HashMap<String,Object> register(HttpServletRequest request){
-        HashMap<String,Object> response = new HashMap<>();
-        RegistrationRequestRule rule = new RegistrationRequestRule();
+		if (!rule.validate(request)) {
+			response.put("outcome", false);
+			response.put("errors", rule.getErrors());
+			return response;
+		}
 
-        if(!rule.validate(request)){
-            response.put("outcome",false);
-            response.put("errors",rule.getErrors());
-            return response;
-        }
+		HashMap<String, Object> data = new HashMap<>();
 
+		data.put("id", HashService.generateSHA1(TimeService.now().toString() + request.getParameter("email")));
+		data.put("email", request.getParameter("email"));
+		data.put("firstname", request.getParameter("firstname"));
+		data.put("surname", request.getParameter("surname"));
+		data.put("password", request.getParameter("password"));
+		data.put("isAdministrator", "0");
+		data.put("registrationDate", TimeService.now().toString());
 
-        HashMap<String,Object> data = new HashMap<>();
+		if (User.insert("User", data)) {
+			response.put("outcome", true);
+			response.put("message", "User successfully registered");
+		} else {
+			response.put("outcome", false);
+			response.put("errors", "Failed to register user, please contact support!");
+		}
 
-        data.put("id", HashService.generateSHA1(TimeService.now().toString()+request.getParameter("email")));
-        data.put("email",request.getParameter("email"));
-        data.put("firstname",request.getParameter("firstname"));
-        data.put("surname",request.getParameter("surname"));
-        data.put("password",request.getParameter("password"));
-        data.put("isAdministrator","0");
-        data.put("registrationDate",TimeService.now().toString());
-
-        if(User.insert("User",data)){
-            response.put("outcome", true);
-            response.put("message","User successfully registered");
-        }else{
-            response.put("outcome", false);
-            response.put("errors","Failed to register user, please contact support!");
-        }
-
-        return response;
-    }
+		return response;
+	}
 }
