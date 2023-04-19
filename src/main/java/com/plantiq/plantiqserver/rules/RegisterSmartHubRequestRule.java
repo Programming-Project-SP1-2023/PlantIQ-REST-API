4f6021cc7cf85b63684b60078bf55560a8b67f61
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-19 21:55:22 +1000

    - Created Registration Request rule for when a user is registering a smart home hub.

---
+++ src/main/java/com/plantiq/plantiqserver/rules/RegisterSmartHubRequestRule.java
@@ -0,0 +1,31 @@
+package com.plantiq.plantiqserver.rules;
+
+import com.plantiq.plantiqserver.core.Rule;
+
+import java.util.ArrayList;
+
+public class RegisterSmartHubRequestRule extends Rule {
+
+    @Override
+    protected void setup() {
+
+        ArrayList<String> id = new ArrayList<>();
+
+        id.add("required");
+        id.add("min:12");
+        id.add("max:17");
+        id.add("!unique:AwaitingRegistration.id");
+
+        this.rules.put("id",id);
+
+        ArrayList<String> name = new ArrayList<>();
+
+        name.add("required");
+        name.add("min:5");
+        name.add("max:50");
+
+        this.rules.put("name",name);
+
+    }
+
+}
