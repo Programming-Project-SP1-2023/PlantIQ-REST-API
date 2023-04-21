ecc4f9ad71ca35566e040526870dbc7c267cf992
Falcons34 <89521528+Falcons34@users.noreply.github.com>
2023-04-21 23:04:32 +1000

    cross

--- src/test/java/com/plantiq/plantiqserver/controllers/AuthenticationControllerTest.java
+++ src/test/java/com/plantiq/plantiqserver/controllers/AuthenticationControllerTest.java
@@ -1,11 +1,6 @@
 package com.plantiq.plantiqserver.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.mock.web.MockHttpServletRequest;
-
-import java.util.HashMap;
 
 class AuthenticationControllerTest {
 	@BeforeEach
@@ -13,33 +8,33 @@ class AuthenticationControllerTest {
 
 	}
 
-	@Test
-	void login() {
-	}
-
-	@Test
-	void logout() {
-	}
-
-	@Test
-	void validate() {
-	}
-
-	/**
-	 * Test the registration endpoint.
-	 * User should be able to register with a valid email and password.
-	 */
-	//@Test
-	//void register() {
-	//	HttpServletRequest data = new MockHttpServletRequest();
-	//	data.setAttribute("firstname", "Bill123");
-	//	data.setAttribute("surname", "Smith");
-	//	data.setAttribute("email", "bill@gmail.com");
-	//	data.setAttribute("password", "password1234");
-	//	// post request to /register
-	//	AuthenticationController controller = new AuthenticationController();
-	//	HashMap<String, Object> res = controller.register(data);
-	///	// assert that the response is successful
-	//	assert res.get("success").equals(true);
-	//}
+//	@Test
+//	void login() {
+//		HttpServletRequest data = new MockHttpServletRequest();
+//		data.setAttribute("email", "briancusack78@gmail.com");
+//		data.setAttribute("password", "Admin1234!");
+//		// post request to /login
+//		AuthenticationController controller = new AuthenticationController();
+//		HashMap<String, Object> res = controller.login(data);
+//		// assert that the response is successful
+//		assert res.get("success").equals(true);
+//	}
+//
+//	/**
+//	 * Test the registration endpoint.
+//	 * User should be able to register with a valid email and password.
+//	 */
+//	@Test
+//	void register() {
+//		HttpServletRequest data = new MockHttpServletRequest();
+//		data.setAttribute("firstname", "Bill123");
+//		data.setAttribute("surname", "Smith");
+//		data.setAttribute("email", "bill@gmail.com");
+//		data.setAttribute("password", "password1234");
+//		// post request to /register
+//		AuthenticationController controller = new AuthenticationController();
+//		HashMap<String, Object> res = controller.register(data);
+//		// assert that the response is successful
+//		assert res.get("success").equals(true);
+//	}
 }
---
+++ src/test/java/com/plantiq/plantiqserver/service/HashServiceTest.java
@@ -0,0 +1,9 @@
+package com.plantiq.plantiqserver.service;
+
+import org.junit.jupiter.api.Test;
+
+class HashServiceTest {
+	@Test
+	void generateSHA1() {
+	}
+}
