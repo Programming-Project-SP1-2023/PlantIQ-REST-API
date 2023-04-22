ca99e443b5eccf9397d28da4b7a6ea600f9273c2
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-22 10:25:50 +1000

    - Finished /{id}/rate endpoint, this now accepts a smart hub ID has part of the URI and will return the current rate for it. - Added code commenting to /all endpoint - Added code commenting to /register endpoint - Updated CORS headers to now allow all methods.

--- src/main/java/com/plantiq/plantiqserver/controllers/SmartHubController.java
+++ src/main/java/com/plantiq/plantiqserver/controllers/SmartHubController.java
@@ -3,7 +3,6 @@ package com.plantiq.plantiqserver.controllers;
 import com.plantiq.plantiqserver.core.Gate;
 import com.plantiq.plantiqserver.model.AwaitingRegistration;
 import com.plantiq.plantiqserver.model.SmartHomeHub;
-import com.plantiq.plantiqserver.model.User;
 import com.plantiq.plantiqserver.rules.RegisterSmartHubRequestRule;
 import com.plantiq.plantiqserver.service.HashService;
 import com.plantiq.plantiqserver.service.TimeService;
@@ -16,7 +15,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PATCH, RequestMethod.PUT, RequestMethod.OPTIONS})
 @RequestMapping("/smarthub")
 public class SmartHubController {
 
@@ -67,12 +66,27 @@ public class SmartHubController {
 
         HashMap<String, Object> response = new HashMap<>();
 
+        //Authenticate the user with the gate service
         if(!Gate.authorized(request)){
             return Gate.abortUnauthorized();
         }
 
+        //Get the current hub requested
+        SmartHomeHub hub = SmartHomeHub.collection().where("id",id).where("user_id",Gate.getCurrentUser().getId()).getFirst();
 
-        return new ResponseEntity<>(response, HttpStatusCode.valueOf(200));
+        int outcome;
+        if(hub == null){
+            outcome = 404;
+            response.put("outcome",false);
+            response.put("error","Smart Home Hub not found, please contact support");
+        }else{
+            outcome = 200;
+            response.put("outcome",true);
+            response.put("rate",hub.getPostFrequency());
+        }
+
+
+        return new ResponseEntity<>(response, HttpStatusCode.valueOf(outcome));
 
     }
 
@@ -81,21 +95,25 @@ public class SmartHubController {
 
         HashMap<String, Object> response = new HashMap<>();
 
+        //Authenticate the user with the gate service
         if(!Gate.authorized(request)){
             return Gate.abortUnauthorized();
         }
 
+        //Get the list of hubs for this current user from the database
         ArrayList<SmartHomeHub> hubs = SmartHomeHub.collection().where("user_id",Gate.getCurrentUser().getId()).get();
 
+        //Check if the user has any hubs by the size and either add them or a message.
         if(hubs.size() == 0){
             response.put("message","No hubs currently registered with user");
         }else{
             response.put("list",hubs);
         }
 
+        //Add a valid response outcome
         response.put("outcome",true);
 
-
+        //Lastly return the response.
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(200));
     }
 
@@ -104,16 +122,20 @@ public class SmartHubController {
 
         HashMap<String, Object> response = new HashMap<>();
 
+        //Authenticate the user with the gate service
         if(!Gate.authorized(request)){
             return Gate.abortUnauthorized();
         }
 
+        //Create our request rule
         RegisterSmartHubRequestRule rule = new RegisterSmartHubRequestRule();
 
+        //Validate our request rule
         if(!rule.validate(request)){
             return rule.abort();
         }
 
+        //Create our new model data hashmap
         HashMap<String,Object> data = new HashMap<>();
 
         data.put("id",request.getParameter("id"));
@@ -123,11 +145,16 @@ public class SmartHubController {
         data.put("lastPosted",TimeService.now());
         data.put("postFrequency","");
 
+        //Get the current object from the awaiting registration table
         AwaitingRegistration smartHubAwaitingRegistration = AwaitingRegistration.collection().where("id",request.getParameter("id")).getFirst();
+
+        //Delete the object from the table by its id.
         smartHubAwaitingRegistration.delete("id");
 
+        //Declare our status outcome
         int outcome;
 
+        //Attempt to insert the data, if true return so, else return false.
         if (SmartHomeHub.insert("SmartHomeHub",data)) {
             response.put("outcome", true);
             response.put("message", "Smart Hub registered to account");
