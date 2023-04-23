f3c1631d014c2097dd93c354d8b1159bfa6dbc49
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-23 23:36:12 +1000

    - Added new GET route for the user controller "/user/session/{token}", this will return the user for that current token if valid.

--- src/main/java/com/plantiq/plantiqserver/controllers/UserController.java
+++ src/main/java/com/plantiq/plantiqserver/controllers/UserController.java
@@ -1,14 +1,18 @@
 package com.plantiq.plantiqserver.controllers;
 
 import com.plantiq.plantiqserver.model.PasswordResetToken;
+import com.plantiq.plantiqserver.model.Session;
 import com.plantiq.plantiqserver.model.User;
 import com.plantiq.plantiqserver.rules.ConsumePasswordResetTokenRule;
 import com.plantiq.plantiqserver.rules.SessionValidateRequestRule;
 import com.plantiq.plantiqserver.service.HashService;
 import com.plantiq.plantiqserver.service.TimeService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Time;
 import java.util.HashMap;
 import java.util.List;
 
@@ -17,6 +21,27 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
+
+        @GetMapping("/session/{token}")
+        public ResponseEntity<HashMap<String, Object>> getUserBySession(@PathVariable("token") String token){
+            HashMap<String,Object> response = new HashMap<>();
+
+            Session session = Session.collection().where("token",token).getFirst();
+
+            int outcome;
+
+            if(session != null && session.getExpiry() > TimeService.now()){
+                response.put("outcome",true);
+                response.put("data",User.collection().where("id",session.getUserId()).getFirst());
+                outcome =  200;
+            }else{
+                response.put("outcome",false);
+                response.put("message","Session token is invalid or expired");
+                outcome = 401;
+            }
+
+            return new ResponseEntity<>(response, HttpStatusCode.valueOf(outcome));
+        }
 
         @GetMapping("/all")
         public List<User> all(){
