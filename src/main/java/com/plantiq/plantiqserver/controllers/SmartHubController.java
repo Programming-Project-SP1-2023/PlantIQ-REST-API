c8c26bf0895abbbbacc21ab2dfd5e21a8c40501f
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-23 11:01:08 +1000

    - Added awaiting registration check to the smart hub controller class. - Added rule validation. - Added logic to save the incoming request data if validated.

--- src/main/java/com/plantiq/plantiqserver/controllers/SmartHubController.java
+++ src/main/java/com/plantiq/plantiqserver/controllers/SmartHubController.java
@@ -2,8 +2,10 @@ package com.plantiq.plantiqserver.controllers;
 
 import com.plantiq.plantiqserver.core.Gate;
 import com.plantiq.plantiqserver.model.AwaitingRegistration;
+import com.plantiq.plantiqserver.model.PlantData;
 import com.plantiq.plantiqserver.model.SmartHomeHub;
 import com.plantiq.plantiqserver.rules.RegisterSmartHubRequestRule;
+import com.plantiq.plantiqserver.rules.SmartHubPostDataRule;
 import com.plantiq.plantiqserver.service.HashService;
 import com.plantiq.plantiqserver.service.TimeService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -31,21 +33,61 @@ public class SmartHubController {
             return new ResponseEntity<>(response, HttpStatusCode.valueOf(400));
         }
 
+        AwaitingRegistration awaitingRegistration = AwaitingRegistration.collection().where("id",id).getFirst();
+
+        if(awaitingRegistration != null){
+            response.put("outcome",true);
+            response.put("message","Awaiting registration");
+            return new ResponseEntity<>(response, HttpStatusCode.valueOf(200));
+        }
+
         //Get the smart home hub model object for this id.
         SmartHomeHub smartHomeHub = SmartHomeHub.collection().where("id",id).getFirst();
 
         //Check the ID is valid and if not set it to await registration.
-        if(smartHomeHub == null && AwaitingRegistration.collection().where("id",id).getFirst() == null){
+        if(smartHomeHub == null){
             HashMap<String,Object> data = new HashMap<>();
             data.put("id",id);
             data.put("date", TimeService.nowPlusDays(7));
             AwaitingRegistration.insert("AwaitingRegistration",data);
+
+            response.put("outcome",true);
+            response.put("message","Awaiting registration");
+
+            //At this stage we simply want to return.
+            return new ResponseEntity<>(response, HttpStatusCode.valueOf(200));
         }
 
-        //Placeholder!
-        response.put("error","Endpoint awaiting further setup");
+        //If we get to this stage we know the hub is valid and registered, and we then process our data validation.
+        SmartHubPostDataRule rule = new SmartHubPostDataRule();
 
-        return new ResponseEntity<>(response, HttpStatusCode.valueOf(200));
+        if(!rule.validate(request)){
+            return rule.abort();
+        }
+
+        HashMap<String,Object> data = new HashMap<>();
+
+        data.put("smarthomehub_Id",smartHomeHub.getId());
+        data.put("sensor_Id",request.getParameter("sensor_id"));
+        data.put("temperature",request.getParameter("temperature"));
+        data.put("humidity",request.getParameter("humidity"));
+        data.put("light",request.getParameter("light"));
+        data.put("moisture",request.getParameter("moisture"));
+        data.put("timestamp",TimeService.now().toString());
+
+        int outcome;
+        if(PlantData.insert("PlantData",data)){
+            response.put("outcome",true);
+            outcome = 200;
+        }else{
+            response.put("error", "Failed to save plant data, please contact support!");
+            response.put("outcome",false);
+            outcome = 500;
+        }
+
+
+
+        return new ResponseEntity<>(response, HttpStatusCode.valueOf(outcome));
     }
 
     @DeleteMapping("/{id}/data")
@@ -163,6 +205,30 @@ public class SmartHubController {
             response.put("outcome", false);
             response.put("errors", "Failed to register smart hub, please contact support!");
             outcome = 500;
+        }
+
+        return new ResponseEntity<>(response, HttpStatusCode.valueOf(outcome));
+    }
+
+    @GetMapping("/{id}/data")
+    public ResponseEntity<HashMap<String,Object>> getData(@PathVariable("id") String id, HttpServletRequest request){
+        HashMap<String, Object> response = new HashMap<>();
+
+        if(!Gate.authorized(request)){
+            return Gate.abortUnauthorized();
+        }
+
+        int outcome;
+
+        if(SmartHomeHub.collection().where("id",id).where("user_id",Gate.getCurrentUser().getId()).getFirst() == null){
+            response.put("outcome",false);
+            response.put("error","Smart Home Hub not found");
+            outcome = 404;
+        }else{
+            response.put("outcome",true);
+            response.put("data",PlantData.collection().where("smarthomehub_Id",id).get());
+            outcome  = 200;
+
         }
 
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(outcome));
