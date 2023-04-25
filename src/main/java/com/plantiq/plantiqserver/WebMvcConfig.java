5e0ace1238e556a25f0badc72dce20e7c1347ae5
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-25 21:41:51 +1000

    - Created WebMvcConfig class, this will set the Cors policy for the App.

---
+++ src/main/java/com/plantiq/plantiqserver/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.plantiq.plantiqserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        System.out.println("[WebMvcConfig] Configured CORS");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("https://plantiq.azurewebsites.net")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(-1);
+    }
+}
