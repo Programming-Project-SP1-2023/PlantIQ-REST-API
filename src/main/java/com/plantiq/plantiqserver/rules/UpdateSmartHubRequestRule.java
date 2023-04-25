e21b30ec257222b364dc67777b4917d735f68ea5
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-04-25 23:11:16 +1000

    - Created UpdateSmartHubRequestRule, this will validate the postFrequency and name prior to saving the data.

---
+++ src/main/java/com/plantiq/plantiqserver/rules/UpdateSmartHubRequestRule.java
@@ -0,0 +1,24 @@
+package com.plantiq.plantiqserver.rules;
+
+import com.plantiq.plantiqserver.core.Rule;
+
+import java.util.ArrayList;
+
+public class UpdateSmartHubRequestRule extends Rule {
+    @Override
+    protected void setup() {
+
+        ArrayList<String> name = new ArrayList<>();
+        name.add("required");
+        name.add("min:3");
+
+        this.rules.put("name",name);
+
+        ArrayList<String> postFrequency = new ArrayList<>();
+        postFrequency.add("required");
+        postFrequency.add("integer");
+
+        this.rules.put("postFrequency",postFrequency);
+
+    }
+}
