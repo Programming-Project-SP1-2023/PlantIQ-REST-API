714556fb8f3c29b53eb62076cd8ba4a19342cd04
s3907260@student@rmit.edu.au <s3907260@student@rmit.edu.au>
2023-05-02 17:57:16 +1000

    Creation of range class, which will be representing the database table in which the ranges for notifications will be stored

---
+++ src/main/java/com/plantiq/plantiqserver/model/Range.java
@@ -0,0 +1,63 @@
+package com.plantiq.plantiqserver.model;
+
+import com.plantiq.plantiqserver.core.Model;
+import com.plantiq.plantiqserver.core.ModelCollection;
+
+import java.util.HashMap;
+
+public class Range extends Model {
+
+    public final static String DEFAULT_TEMPERATURE_RANGE = "40-60";
+    public final static String DEFAULT_HUMIDITY_RANGE ="40-60";
+    public final static String DEFAULT_LIGHT_RANGE ="40-60";
+    public final static String DEFAULT_MOISTURE_RANGE ="40-60";
+
+    //|================================================|
+    //|              STATIC CRUD METHODS               |
+    //|================================================|
+
+    //Our collection method is the only method that has to be
+    //present in the child classes as it must inject the child
+    //class type to ensure they are returned correctly.
+
+    //This method will create a ModelCollection object using
+    //the current type via generics and by providing the class
+    //directly into the constructor, this collection object
+    //allows us to build our SQL query and return our values.
+
+    public static ModelCollection<Range> collection(){
+        return new ModelCollection<>(Range.class);
+    }
+
+
+    //|================================================|
+    //|                  CLASS METHODS                 |
+    //|================================================|
+
+    //Constructor
+    public Range(HashMap<String, Object> data){
+        super(data);
+    }
+
+    //Getters
+    public String getId(){
+        return (String)this.data.get("id");
+    }
+
+    public String getRangeHumidity(){
+        return (String)this.data.get("range_humidity");
+    }
+
+    public String getRangeLight(){
+        return (String)this.data.get("range_light");
+    }
+
+    public String getRangeTemperature(){
+        return (String)this.data.get("range_temperature");
+    }
+
+    public String getRangeMoisture(){
+        return (String)this.data.get("range_moisture");
+    }
+
+}
