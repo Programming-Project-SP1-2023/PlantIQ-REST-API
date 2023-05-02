db40eda7f1a6ee5044a6ab15c843be9b4f742951
s3907260@student@rmit.edu.au <s3907260@student@rmit.edu.au>
2023-05-02 17:55:49 +1000

    Creation of notification class

---
+++ src/main/java/com/plantiq/plantiqserver/model/Notifications.java
@@ -0,0 +1,53 @@
+package com.plantiq.plantiqserver.model;
+
+import com.plantiq.plantiqserver.core.Model;
+import com.plantiq.plantiqserver.core.ModelCollection;
+
+import java.util.HashMap;
+public class Notifications extends Model{
+
+    //|================================================|
+    //|              STATIC CRUD METHODS               |
+    //|================================================|
+
+    //Our collection method is the only method that has to be
+    //present in the child classes as it must inject the child
+    //class type to ensure they are returned correctly.
+
+    //This method will create a ModelCollection object using
+    //the current type via generics and by providing the class
+    //directly into the constructor, this collection object
+    //allows us to build our SQL query and return our values.
+
+    public static ModelCollection<Notifications> collection(){
+        return new ModelCollection<>(Notifications.class);
+    }
+
+
+    //|================================================|
+    //|                  CLASS METHODS                 |
+    //|================================================|
+
+    //Constructor
+    public Notifications(HashMap<String, Object> data){
+        super(data);
+    }
+
+    //Getters
+    public String getId(){
+        return (String)this.data.get("id");
+    }
+
+    public String getUserID(){
+        return (String)this.data.get("user_id");
+    }
+
+    public int getTimestamp(){return Integer.parseInt((String)this.data.get("timestamp"));}
+
+    public String getField(){
+        return (String)this.data.get("field");
+    }
+
+    public float getValue(){ return Float.parseFloat((String)this.data.get("value"));}
+
+}
