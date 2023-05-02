5269f7171bad9d4393cb806804ed5129aad325c0
s3907260@student@rmit.edu.au <s3907260@student@rmit.edu.au>
2023-05-03 00:11:33 +1000

    Creation of rule for updating limits

---
+++ src/main/java/com/plantiq/plantiqserver/rules/updateLimitRule.java
@@ -0,0 +1,32 @@
+package com.plantiq.plantiqserver.rules;
+
+import com.plantiq.plantiqserver.core.Rule;
+
+import java.util.ArrayList;
+
+// -----------------------------------------------------------------------------------|
+//                                  LIST OF RULES                                     |
+// -----------------------------------------------------------------------------------|
+//                                                                                    |
+//  Temperature/Humidity/Moisture/Light                                               |
+//  A) range:float[0,100] -> Must be a float between 0 and 100                        |
+//  B) regex.email -> Validates the input meets the criteria of an email address.     |
+//                                                                                    |
+//------------------------------------------------------------------------------------|
+
+public class updateLimitRule extends Rule {
+
+    @Override
+    protected void setup() {
+
+        ArrayList<String> temperature= new ArrayList<>();
+        ArrayList<String> humidity= new ArrayList<>();
+        ArrayList<String> moisture= new ArrayList<>();
+        ArrayList<String> light= new ArrayList<>();
+
+        temperature.add("range:float[0,100]");
+        humidity.add("range:float[0,100]");
+        moisture.add("range:float[0,100]");
+        light.add("range:float[0,100]");
+    }
+}
