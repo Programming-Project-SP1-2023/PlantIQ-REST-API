1593ea4ab5357a67f12cf82482aedee9c579cfc9
Jack Harris <79836947+s3893749@users.noreply.github.com>
2023-05-05 18:36:11 +1000

    - Created new Email class, this will be used to create and send email messages, this class is a replacement for the EmailService. - Added setUser() method, this sets the current user object for the email. - Added setSubject() method, this accepts a subject line as a string and sets the subject. - Added setVariables() method, this allows a hashmap of variables to be provided and will replace those found in the template, all variable names should match except for template ones to contain "{{x}}" curly brackets to indicate its a var. - Added setHtmlTemplate() method, this accepts a uri as a string and sets the html template. - Added send() method, this method will create a new Thread and use it to send the email so the main application will not hang.

---
+++ src/main/java/com/plantiq/plantiqserver/core/Email.java
@@ -0,0 +1,114 @@
+package com.plantiq.plantiqserver.core;
+
+import com.aspose.email.MailAddress;
+import com.aspose.email.MailMessage;
+import com.aspose.email.SecurityOptions;
+import com.aspose.email.SmtpClient;
+import com.plantiq.plantiqserver.PlantIqServerApplication;
+import com.plantiq.plantiqserver.model.User;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Objects;
+
+public class Email {
+
+    private static final HashMap<String, String> templates = new HashMap<>();
+
+    private User user;
+    private String subject;
+
+    private MailMessage message;
+
+    private String template;
+
+    private HashMap<String,Object> variables;
+
+    public Email(){
+        this.variables = new HashMap<>();
+        this.message = new MailMessage();
+        this.message.setFrom(new MailAddress("help-plantiq@outlook.com", "PlantIQ Help", false));
+    }
+
+    public Email setUser(User user){
+        this.user = user;
+        this.message.getTo().addItem(new MailAddress(user.getEmail(), user.getFirstname()+" "+user.getSurname(), false));
+        return this;
+    }
+
+    public Email setSubject(String subject){
+        this.message.setSubject(subject);
+        return this;
+    }
+
+    public Email setVariables(HashMap<String, Object> variables){
+        this.variables = variables;
+        return this;
+    }
+
+    public Email setHtmlTemplate(String template){
+
+        if(!Email.templates.containsKey(template)){
+            try {
+                InputStreamReader inputStreamReader = new InputStreamReader(Objects.requireNonNull(PlantIqServerApplication.class.getResourceAsStream(template)));
+
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                StringBuilder sb = new StringBuilder();
+                String str;
+                while ((str = reader.readLine()) != null) {
+                    sb.append(str);
+                }
+                Email.templates.put(template,sb.toString());
+                this.message.setHtmlBody(sb.toString());
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }else{
+            this.message.setHtmlBody(Email.templates.get(template));
+        }
+
+        return this;
+    }
+
+    public void send(){
+
+        Thread t1 = new Thread(() -> {
+            this.message.setHtmlBody(this.message.getHtmlBody().replace("{{user.fullname}}",this.user.getFirstname()+" "+this.user.getSurname()));
+
+            this.variables.forEach((key,value)->{
+                if(this.message.getHtmlBody().contains("{{"+key+"}}")){
+                    this.message.setHtmlBody(this.message.getHtmlBody().replace("{{"+key+"}}", value.toString()));
+                }
+            });
+
+            // Create an instance of SmtpClient Class
+            SmtpClient client = new SmtpClient();
+
+            // Specify your mailing host server, Username, Password, Port
+            client.setHost("smtp-mail.outlook.com");
+            client.setUsername("help-plantiq@outlook.com");
+            client.setPassword("");
+            client.setPort(587);
+            client.setSecurityOptions(SecurityOptions.Auto);
+
+            try
+            {
+                // Client.Send will send this message
+                client.beginSend(this.message);
+            }
+
+            catch (Exception ex)
+            {
+                ex.printStackTrace();
+            }
+
+        });
+        t1.start();
+        System.out.println("[Email] Sent "+this.message.getSubject()+" email to "+this.user.getEmail());
+    }
+
+
+
+
+
+}
