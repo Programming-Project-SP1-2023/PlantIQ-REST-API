56c2bed73fd331cddc6b37a1ec03ebefcc19555e
s3907260@student@rmit.edu.au <s3907260@student@rmit.edu.au>
2023-05-06 23:41:47 +1000

    Added documentation for CleanExpiredSessionTokensJob

--- src/main/java/com/plantiq/plantiqserver/jobs/CleanExpiredSessionTokensJob.java
+++ src/main/java/com/plantiq/plantiqserver/jobs/CleanExpiredSessionTokensJob.java
@@ -6,13 +6,20 @@ import com.plantiq.plantiqserver.service.TimeService;
 
 import java.util.ArrayList;
 
+//--------------------------------------Job Class--------------------------------------
+//This Job class has been created with the scope of deleting all the session tokens
+//that are expired.
+//-------------------------------------------------------------------------------------
+
 public class CleanExpiredSessionTokensJob extends Job {
 
+    //This method goes through all the sessions stored in the database and deletes those which are expired
     @Override
     public boolean run() {
-
+        //Extraction of sessions which have an expiry date older that the current time
         ArrayList<Session> items = Session.collection().whereGreaterThan("expiry", TimeService.now().toString()).get();
 
+        //Deletion of each session extracted into the list
         items.forEach((n)->{
             n.delete("token");
         });
