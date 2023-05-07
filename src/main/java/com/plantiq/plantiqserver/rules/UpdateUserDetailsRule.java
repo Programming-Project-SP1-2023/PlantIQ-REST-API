1ddc5fe6cc5a1d19c46f5e83550f86b241f81469
s3907260@student@rmit.edu.au <s3907260@student@rmit.edu.au>
2023-05-07 16:39:42 +1000

    Update the rules to make updateUserDetails fields optional

--- src/main/java/com/plantiq/plantiqserver/rules/UpdateUserDetailsRule.java
+++ src/main/java/com/plantiq/plantiqserver/rules/UpdateUserDetailsRule.java
@@ -11,28 +11,27 @@ public class UpdateUserDetailsRule extends Rule {
 
         ArrayList<String> email = new ArrayList<>();
 
-        email.add("required");
-        email.add("regex:email");
-        email.add("@unique:User.email");
+        email.add("?regex:email");
+        email.add("?@unique:User.email");
 
         this.rules.put("email",email);
 
         ArrayList<String> firstname = new ArrayList<>();
-        firstname.add("required");
-        firstname.add("min:3");
-        firstname.add("max:25");
+        firstname.add("?min:3");
+        firstname.add("?max:25");
+
         this.rules.put("firstname",firstname);
 
         ArrayList<String> surname = new ArrayList<>();
-        surname.add("required");
-        surname.add("min:2");
-        surname.add("max:25");
+        surname.add("?min:2");
+        surname.add("?max:25");
+
         this.rules.put("surname",surname);
 
         ArrayList<String> password = new ArrayList<>();
-        password.add("required");
-        password.add("min:8");
-        password.add("max:50");
+        password.add("?min:8");
+        password.add("?max:50");
+
 
         this.rules.put("password",password);
     }
