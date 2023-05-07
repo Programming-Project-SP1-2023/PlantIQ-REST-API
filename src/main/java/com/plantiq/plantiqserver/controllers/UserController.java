f5dc2b165f664c0432b788393a5c6e7768703677
s3907260@student@rmit.edu.au <s3907260@student@rmit.edu.au>
2023-05-07 16:50:10 +1000

    Update code to allow optional fields and added documentation to entire class

--- src/main/java/com/plantiq/plantiqserver/controllers/UserController.java
+++ src/main/java/com/plantiq/plantiqserver/controllers/UserController.java
@@ -20,68 +20,99 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
+//---------------------------------Controller Class---------------------------------
+//The UserController class will be serving all the endpoints that deal user's info.
+//here it will be possible to retrieve the information regarding a single user, a
+//group of users and modify their data.
+//----------------------------------------------------------------------------------
+
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-
+        //@GetMapping("/info") is the endpoint that will return the information
+        //of the logged-in user
         @GetMapping("/info")
         public ResponseEntity<HashMap<String, Object>> getUserBySession(HttpServletRequest request){
+            //Create our response object, this is returned as JSON.
             HashMap<String,Object> response = new HashMap<>();
-
+            //Authenticate the user with the gate service
             if(!Gate.authorized(request)){
                 return Gate.abortUnauthorized();
             }
-
+            //The response returning to the front end will contain all the data of the
+            //logged-in user.
             response.put("outcome",true);
             response.put("data",User.collection().where("id",Gate.getCurrentUser().getId()).getFirst());
 
             return new ResponseEntity<>(response, HttpStatusCode.valueOf(200));
         }
-
+        //@PatchMapping("/info") is the endpoint that will allow the user to modify
+        //their saved personal data. They will be able to change name,surname,email
+        //and password.
         @PatchMapping("/info")
         public ResponseEntity<HashMap<String, Object>> updateUserBySession(HttpServletRequest request){
+            //Create our response object, this is returned as JSON.
             HashMap<String,Object> response = new HashMap<>();
-
+            //Authenticate the user with the gate service
             if(!Gate.authorized(request)){
                 return Gate.abortUnauthorized();
             }
-
+            //Getting the current logged-in user.
+            User user = User.collection().where("id",Gate.getCurrentUser().getId()).getFirst();
+            //Create our update details user rule, we will validate
+            //our request using this rule.
             UpdateUserDetailsRule rule = new UpdateUserDetailsRule();
-
+            //Validate our request and return the errors if present.
             if(!rule.validate(request)){
                 return rule.abort();
             }
 
-            User user = User.collection().where("id",Gate.getCurrentUser().getId()).getFirst();
-
+            //Creating the structure necessary to insert user's details
+            //into the database. Since these fields are optional, in
+            //order to not modify the entire database replacing values
+            //with null, we need to check if these have been submitted
+            //in the request.
             HashMap<String,Object> data = new HashMap<>();
-            data.put("email",request.getParameter("email"));
-            data.put("firstname",request.getParameter("firstname"));
-            data.put("surname",request.getParameter("surname"));
-            data.put("password",HashService.generateSHA1(PlantIqServerApplication.passwordPepper+request.getParameter("password")));
+            if(request.getParameter("email")!=null)
+                data.put("email",request.getParameter("email"));
+            if(request.getParameter("firstname")!=null)
+                data.put("firstname",request.getParameter("firstname"));
+            if(request.getParameter("surname")!=null)
+                data.put("surname",request.getParameter("surname"));
+            if(request.getParameter("password")!=null)
+                data.put("password",HashService.generateSHA1(PlantIqServerApplication.passwordPepper+request.getParameter("password")));
 
+            //Updating the entity User with the new data.
             user.update(data);
-
+            //A successful message will be returned once the entity
+            //has been updated.
             response.put("outcome",true);
             response.put("message","User details updated");
 
             return new ResponseEntity<>(response, HttpStatusCode.valueOf(200));
         }
-
+        //@GetMapping("/all") is the endpoint which will return all records
+        //of the user's entity. It is possible to refine the search by limiting,
+        //offsetting and sorting the returned records.
         @GetMapping("/all")
         public ResponseEntity<HashMap<String, Object>> all(HttpServletRequest request){
+            //Create our response object, this is returned as JSON.
             HashMap<String,Object> response = new HashMap<>();
-
+            //Authenticate the user with the gate service
             if(!Gate.authorizedAsAdmin(request)){
                 return Gate.abortUnauthorized();
             }
-
+            //Create our get all rule, we will validate
+            //our request using this rule.
             GetAllRule rule = new GetAllRule();
+            //Validate our request and return the errors if present.
             if(!rule.validate(request)){
                 return rule.abort();
             }
 
+            //Creating the user's modelCollection
             ModelCollection<User> users = User.collection();
 
             //Add all the required parameters.
@@ -90,6 +121,8 @@ public class UserController {
             users.orderBy(request.getParameter("sortBy"));
             users.orderType(Sort.valueOf(request.getParameter("sort")));
 
+            //As result of this operation, a list of users will be returned,
+            //with a successful message and outcome.
             response.put("list",users.get());
             response.put("outcome",true);
             response.put("message","successfully processed request");
