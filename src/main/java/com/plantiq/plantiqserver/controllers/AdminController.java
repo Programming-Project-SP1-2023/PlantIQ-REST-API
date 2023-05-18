764ccd5b69974df21d169f25a8df4ab910d09755
Jack Harris <s3893749@student.rmit.edu.au>
2023-05-18 14:23:12 +0100

    - Created new AdminController, this will contain all our admin functions. - Created /admin/user/activate/{id} POST endpoint, this will allow an admin to activate a users account. - Migrated /user/all GET endpoint from the UserController as this is an admin only endpoint.

---
+++ src/main/java/com/plantiq/plantiqserver/controllers/AdminController.java
@@ -0,0 +1,96 @@
+package com.plantiq.plantiqserver.controllers;
+
+import com.plantiq.plantiqserver.core.Gate;
+import com.plantiq.plantiqserver.core.ModelCollection;
+import com.plantiq.plantiqserver.core.Sort;
+import com.plantiq.plantiqserver.model.User;
+import com.plantiq.plantiqserver.rules.GetAllRule;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+
+    @PostMapping("/user/activate/{id}")
+    public ResponseEntity<HashMap<String,Object>> activateAccount(@PathVariable("id") String id, HttpServletRequest request){
+
+        HashMap<String,Object> response = new HashMap<>();
+
+        if(!Gate.authorizedAsAdmin(request)){
+            return Gate.abortUnauthorized();
+        }
+
+        User user = User.collection().where("id",id).getFirst();
+
+        if(user == null){
+            response.put("outcome",false);
+            response.put("message","User account not found");
+            return new ResponseEntity<>(response, HttpStatusCode.valueOf(404));
+        }
+
+
+        int outcome = 200;
+
+        if(user.isActivated()){
+            response.put("outcome",true);
+            response.put("message","User account is already activated");
+        }else{
+            HashMap<String,Object> data = new HashMap<>();
+            data.put("isActivated","1");
+
+            if(user.update(data)){
+                response.put("outcome",true);
+                response.put("message","Successfully activated user account");
+            }else{
+                outcome = 500;
+                response.put("message","Failed to activate user account, please contact support");
+            }
+        }
+
+        return new ResponseEntity<>(response, HttpStatusCode.valueOf(outcome));
+    }
+
+    //@GetMapping("/all") is the endpoint which will return all records
+    //of the user's entity. It is possible to refine the search by limiting,
+    //offsetting and sorting the returned records.
+    @GetMapping("/user/all")
+    public ResponseEntity<HashMap<String, Object>> all(HttpServletRequest request) {
+        //Create our response object, this is returned as JSON.
+        HashMap<String, Object> response = new HashMap<>();
+        //Authenticate the user with the gate service
+        if (!Gate.authorizedAsAdmin(request)) {
+            return Gate.abortUnauthorized();
+        }
+        //Create our get all rule, we will validate
+        //our request using this rule.
+        GetAllRule rule = new GetAllRule();
+        //Validate our request and return the errors if present.
+        if (!rule.validate(request)) {
+            return rule.abort();
+        }
+
+        //Creating the user's modelCollection
+        ModelCollection<User> users = User.collection();
+
+        //Add all the required parameters.
+        users.limit(Integer.parseInt(request.getParameter("limit")));
+        users.offset(Integer.parseInt(request.getParameter("offset")));
+        users.orderBy(request.getParameter("sortBy"));
+        users.orderType(Sort.valueOf(request.getParameter("sort")));
+
+        //As result of this operation, a list of users will be returned,
+        //with a successful message and outcome.
+        response.put("list", users.get());
+        response.put("outcome", true);
+        response.put("message", "successfully processed request");
+
+        return new ResponseEntity<>(response, HttpStatusCode.valueOf(200));
+    }
+
+}
