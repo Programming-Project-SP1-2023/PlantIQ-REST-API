342d1968c974c3481ba2b942c2d6db569ec162ba
Jack Harris <s3893749@student.rmit.edu.au>
2023-05-18 15:34:59 +0100

    - Added delete method for all plant data inside a smart home hub.

--- src/main/java/com/plantiq/plantiqserver/controllers/AdminController.java
+++ src/main/java/com/plantiq/plantiqserver/controllers/AdminController.java
@@ -192,4 +192,36 @@ public class AdminController {
 
         return new ResponseEntity<>(response,HttpStatusCode.valueOf(200));
     }
+
+    @DeleteMapping("/user/{id}/smarthomehub/{hub_id}/data")
+    public ResponseEntity<HashMap<String,Object>> deleteSmartHubData(HttpServletRequest request, @PathVariable("id") String id, @PathVariable String hub_id){
+        HashMap<String,Object> response = new HashMap<>();
+
+        if(!Gate.authorizedAsAdmin(request)){
+            return Gate.abortUnauthorized();
+        }
+
+        User user = User.collection().where("id",id).getFirst();
+
+        if(user == null){
+            response.put("outcome",false);
+            response.put("message","User not found");
+            return new ResponseEntity<>(response,HttpStatusCode.valueOf(404));
+        }
+
+        SmartHomeHub hub = SmartHomeHub.collection().where("user_id",id).where("id",hub_id).getFirst();
+
+        if(hub == null){
+            response.put("outcome",false);
+            response.put("message","Smart home hub not found");
+            return new ResponseEntity<>(response,HttpStatusCode.valueOf(404));
+        }
+
+        PlantData.deleteAll("PlantData","smarthomehub_id",hub.getId());
+
+        response.put("outcome",true);
+        response.put("message","Smart home hub deleted");
+
+        return new ResponseEntity<>(response,HttpStatusCode.valueOf(200));
+    }
 }
