87cab53f4462ba61b3730a8e4d9e45236b8c2a9c
Jack Harris <s3893749@student.rmit.edu.au>
2023-05-18 15:23:11 +0100

    - Updated spacing.

--- src/main/java/com/plantiq/plantiqserver/service/PlantService.java
+++ src/main/java/com/plantiq/plantiqserver/service/PlantService.java
@@ -10,25 +10,25 @@ import java.util.HashMap;
 
 public class PlantService {
 
-    public static ArrayList<HashMap<String, Object>> getAll(){
-        ArrayList<HashMap<String,Object>> response = new ArrayList<>();
+    public static ArrayList<HashMap<String, Object>> getAll() {
+        ArrayList<HashMap<String, Object>> response = new ArrayList<>();
 
         ArrayList<String> tracker = new ArrayList<>();
 
         ArrayList<SmartHomeHub> hubs = SmartHomeHub.collection().where("user_id", Gate.getCurrentUser().getId()).get();
 
-        hubs.forEach((n)->{
-            ArrayList<PlantData> plantData = PlantData.collection().where("smarthomehub_id",n.getId()).get();
+        hubs.forEach((n) -> {
+            ArrayList<PlantData> plantData = PlantData.collection().where("smarthomehub_id", n.getId()).get();
 
-            plantData.forEach((p)->{
-                if(!tracker.contains(p.getSensorId())){
+            plantData.forEach((p) -> {
+                if (!tracker.contains(p.getSensorId())) {
                     tracker.add(p.getSensorId());
 
-                    HashMap<String,Object> plant = new HashMap<>();
-                    plant.put("sensor_id",p.getSensorId());
+                    HashMap<String, Object> plant = new HashMap<>();
+                    plant.put("sensor_id", p.getSensorId());
                     plant.put("name", PlantService.getNameOrId(p.getSensorId()));
-                    plant.put("smarthomehub_id",p.getSmartHomeHubId());
-                    plant.put("postFrequency",n.getPostFrequency());
+                    plant.put("smarthomehub_id", p.getSmartHomeHubId());
+                    plant.put("postFrequency", n.getPostFrequency());
 
                     response.add(plant);
 
@@ -40,20 +40,20 @@ public class PlantService {
         return response;
     }
 
-    public static boolean validPlantId(String id, String user_id){
+    public static boolean validPlantId(String id, String user_id) {
 
-        PlantData plantData = PlantData.collection().where("sensor_id",id).getFirst();
+        PlantData plantData = PlantData.collection().where("sensor_id", id).getFirst();
 
         return plantData != null;
 
     }
 
-    public static String getNameOrId(String sensor_id){
-        PlantName plantName = PlantName.collection().where("sensor_id",sensor_id).orderBy("name").getFirst();
+    public static String getNameOrId(String sensor_id) {
+        PlantName plantName = PlantName.collection().where("sensor_id", sensor_id).orderBy("name").getFirst();
 
-        if(plantName == null){
+        if (plantName == null) {
             return sensor_id;
-        }else{
+        } else {
             return plantName.getName();
         }
     }
