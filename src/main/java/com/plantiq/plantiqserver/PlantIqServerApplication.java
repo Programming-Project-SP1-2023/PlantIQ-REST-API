ae118d0ebc699dab7d5063f67766f487b5155fb9
Jack Harris <s3893749@student.rmit.edu.au>
2023-05-18 11:00:14 +0100

    - Created all getters and internal variables for our details loaded from the application.properties file.

--- src/main/java/com/plantiq/plantiqserver/PlantIqServerApplication.java
+++ src/main/java/com/plantiq/plantiqserver/PlantIqServerApplication.java
@@ -1,6 +1,7 @@
 package com.plantiq.plantiqserver;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,12 +9,73 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlantIqServerApplication {
 
-	public static final String passwordPepper = "Steak&Chips!";
-	public static final String emailPassword = "";
-	public static final String databasePassword = "";
+	@Value("${database.password}")
+	private String databaseConnectionLink;
+
+	@Value("${password.pepper}")
+	private String passwordPepper;
+
+	@Value("${email.password}")
+	private String emailPassword;
+
+	@Value("${email.username}")
+	private String emailUsername;
+
+	@Value("${email.host}")
+	private String emailHost;
+
+	@Value("${email.port}")
+	private int emailPort;
+
+	@Value("${email.displayName}")
+	private String emailDisplayName;
+
+
+	private static PlantIqServerApplication instance;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(PlantIqServerApplication.class, args);
+	}
+
+	public PlantIqServerApplication(){
+
+		PlantIqServerApplication.instance = this;
+	}
+
+	public String getDatabasePassword(){
+		return this.databaseConnectionLink;
+	}
+
+	public String getEmailUsername(){
+		return this.emailUsername;
+	}
+
+	public String getEmailPassword(){
+		return this.emailPassword;
+	}
+
+	public int getEmailPort(){
+		return this.emailPort;
+	}
+
+	public String getEmailHost(){
+		return this.emailHost;
+	}
+
+	public String getEmailDisplayName(){
+		return this.emailDisplayName;
+	}
+
+	public String getPasswordPepper(){
+		return this.passwordPepper;
+	}
+
+	public String getDatabaseConnectionLink(){
+		return this.databaseConnectionLink;
+	}
+
+	public static PlantIqServerApplication getInstance(){
+		return PlantIqServerApplication.instance;
 	}
 }
