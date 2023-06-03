69301e19bb1e3419ec469c0b4d57ad95ea8c8a5e
s3907260 <80504179+s3907260@users.noreply.github.com>
2023-06-03 11:46:44 +0200

    Update comments for PlantController

--- src/main/java/com/plantiq/plantiqserver/controllers/PlantController.java
+++ src/main/java/com/plantiq/plantiqserver/controllers/PlantController.java
@@ -43,8 +43,9 @@ public class PlantController {
     //
     @GetMapping("/{id}/name")
     public ResponseEntity<HashMap<String,Object>> getPlantName(@PathVariable("id") String id, HttpServletRequest request){
+        //Create our response object, this is returned as JSON.
         HashMap<String,Object> response = new HashMap<>();
-
+        //Authenticate the user with the gate service
         if(!Gate.authorized(request)){
             return Gate.abortUnauthorized();
         }
@@ -57,14 +58,17 @@ public class PlantController {
         //If the plant is null, no plant with the given ID was found.
         //A false outcome, a 404 error and an error message will be
         //returned. If a plant with the given id does exist, the
-        //outcome will be true and a 200 status code will be returned.
+        //outcome will be true, a 200 status code will be returned
+        // and the plant name will be added to the response.
         if(plantData == null){
             response.put("outcome",false);
             response.put("message","No record found of plant for the provided id");
             outcome = 404;
         }else{
             response.put("outcome",true);
-            
+            //If a plant does have a name, it will be used.
+            //If there is no name for that plant, it's ID
+            //will be used
             if(plantName == null){
                 response.put("name",plantData.getSensorId());
                 response.put("message","No display name set, defaulting to sensor_id value");
@@ -76,39 +80,50 @@ public class PlantController {
 
         return new ResponseEntity<>(response, HttpStatusCode.valueOf(outcome));
     }
-
+    //@PatchMapping("/{id}/name") is the endpoint used to change the name of a plant.
     @PatchMapping("/{id}/name")
     public ResponseEntity<HashMap<String,Object>> updatePlantName(@PathVariable("id") String id, HttpServletRequest request){
+        //Create our response object, this is returned as JSON.
         HashMap<String,Object> response = new HashMap<>();
-
+        //Authenticate the user with the gate service
         if(!Gate.authorized(request)){
             return Gate.abortUnauthorized();
         }
-
+        //Create our update plant name rule, we will validate
+        //our request using this rule.
         UpdatePlantNameRule rule = new UpdatePlantNameRule();
+        //Validate our request and return the errors if present.
         if(!rule.validate(request)){
             return rule.abort();
         }
 
-
+        //If statement to ensure that a plant with the given ID does exist.
+        //If no record was found, a false outcome will be returned with an
+        //error message.
         if(!PlantService.validPlantId(id)){
             response.put("outcome",false);
             response.put("message","No record found of plant for the provided id");
             return new ResponseEntity<>(response, HttpStatusCode.valueOf(404));
         }
-
+        //Retrieving the old name
         PlantName old = PlantName.collection().where("user_id",Gate.getCurrentUser().getId()).where("sensor_id",id).orderBy("name").getFirst();
-
+        //If the plant had a name, its record will be deleted.
         if(old != null){
             old.delete("sensor_id");
         }
 
         HashMap<String,Object> data = new HashMap<>();
+        //Retrieving the new name, the sensor's id and the user's id.
         data.put("name",request.getParameter("name"));
         data.put("user_id",Gate.getCurrentUser().getId());
         data.put("sensor_id",id);
-
+        //Variable that will store the html status code.
         int outcome;
+        //Attemp to insert the plant's name into the database.
+        //If the record is inserted successfully, a true outcome 
+        //and a 200 http status code will be returned. If the 
+        //query did not affect the database, a 500 error will be
+        //thrown with a message and a false outcome.
         if(PlantName.insert("PlantName",data)){
             outcome = 200;
             response.put("outcome",true);
